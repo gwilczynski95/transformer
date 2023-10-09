06c971b10facdeb076db297553fb73c08035d7c7
gwilczynski95 <g.wilczynski95@gmail.com>
2023-10-09 23:01:34 +0200

    Add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='transformer',
+    version='0.0.1',
+    packages=['transformer'],
+    url='',
+    license='MIT',
+    author='g.wilczynski95',
+    author_email='g.wilczynski95@gmail.com',
+    description=''
+)
